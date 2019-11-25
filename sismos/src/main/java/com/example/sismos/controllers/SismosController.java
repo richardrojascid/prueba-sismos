75f0ee18fe816eead2151e4fea3852b6bcdf4e73
Inovabiz <Inovabiz@LAPTOP-17H19GVH>
2019-11-25 17:44:55 -0300

    terminado servicios y controller con todos los endPoints

--- sismos/src/main/java/com/example/sismos/controllers/SismosController.java
+++ sismos/src/main/java/com/example/sismos/controllers/SismosController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.sismos.model.Sismos;
@@ -27,21 +28,20 @@ public class SismosController {
 	
 	
 	/**
-	 * Método que consume el EndPoint WEB mediante servicio sismosService y publica la información de sismos
+	 * Método mediante servicio sismosService y publica la información de sismos
 	 * por fechas en el EndPoit local.
 	 * @param fechaInicio
 	 * @param fechaTermino
 	 * @return
 	 */
-	@RequestMapping(method = RequestMethod.GET, path = "/buscar/fechas/{fechaInicio}/{fechaTermino}")
-	public ResponseEntity<?> sismosPorRangoFecha(@PathVariable(name = "fechaInicio") String fechaInicio,
-												 @PathVariable(name = "fechaTermino") String fechaTermino) {
-		return new ResponseEntity<Sismos>(sismosService.getSismosByRangoFecha(fechaInicio, fechaTermino),
+	@RequestMapping(method = RequestMethod.GET, path = "/buscar/fechas/{fecha1}/{fecha2}")
+	public ResponseEntity<?> sismosPorFecha(@PathVariable(name = "fecha1") String fecha1,
+												 @PathVariable(name = "fecha2") String fecha2) {
+		return new ResponseEntity<Sismos>(sismosService.getSismosByDate(fecha1, fecha2),
 				HttpStatus.OK);
 	}
-	
 	/**
-	 * Método que consume el EndPoint WEB, mediante servicio sismosService y publica la información de sismos
+	 * Método publica la información de sismos
 	 * con dos magnitudes en el EndPoint local.
 	 * @param minMagnitud
 	 * @param maxMagnitud
@@ -49,13 +49,12 @@ public class SismosController {
 	 */
 	@RequestMapping(method = RequestMethod.GET, path = "/buscar/magnitudes/{minMagnitud}/{maxMagnitud}")
 	public ResponseEntity<?> getSismosByMagnitude(@PathVariable(name = "minMagnitud") String minMagnitud,
-											   @PathVariable(name = "maxMagnitud") String maxMagnitud) {
+											   	  @PathVariable(name = "maxMagnitud") String maxMagnitud) {
 		return new ResponseEntity<Sismos>(sismosService.getSismosByMagnitude(minMagnitud, maxMagnitud),
 				HttpStatus.OK);
 	}
-
 	/**
-	 * Método que consume el EndPoint WEB, mediante servicio sismosService y publica la información de sismos
+	 * Método mediante servicio sismosService publica la información de sismos
 	 * por País en el EndPoint local.
 	 * @param pais
 	 * @return: Información de sismos por País.
@@ -64,7 +63,14 @@ public class SismosController {
 	public ResponseEntity<?> sismosPorPais(@PathVariable(name = "pais") String pais) {
 		return new ResponseEntity<>(sismosService.getSismosByPais(pais), HttpStatus.OK);
 	}
-
+	/**
+	 * resultados por rango de 4 fechas 2 de inicio y dos de termino
+	 * @param fechaInicioR1
+	 * @param fechaTerminoR1
+	 * @param fechaInicioR2
+	 * @param fechaTerminoR2
+	 * @return todos los sismos por rangos
+	 */
 	@RequestMapping(method = RequestMethod.GET, path = "/buscar/Dosfechas/{fechaInicioR1}/{fechaTerminoR1}/{fechaInicioR2}/{fechaTerminoR2}")
 	public ResponseEntity<?> getSismosByTwoDateRanges(@PathVariable(name = "fechaInicioR1") String fechaInicioR1,
 												  	  @PathVariable(name = "fechaTerminoR1") String fechaTerminoR1,
@@ -73,4 +79,38 @@ public class SismosController {
 		return new ResponseEntity<Sismos>((Sismos) sismosService.getSismosByTwoDateRanges(fechaInicioR1, fechaTerminoR1,fechaInicioR2,fechaTerminoR2),
 				HttpStatus.OK);
 	}
+	/**
+	 * resultado por magnitudes
+	 * @param minMagnitudR1
+	 * @param maxMagnitudR1
+	 * @param minMagnitudR2
+	 * @param maxMagnitudR2
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.GET, path = "/buscar/magnitudes/{minMagnitudR1}/{maxMagnitudR1}/{minMagnitudR2}/{maxMagnitudR2}")
+	public ResponseEntity<?> getSismosByFourMagnitudes(@PathVariable(name = "minMagnitudR1") String minMagnitudR1,
+													   @PathVariable(name = "maxMagnitudR1") String maxMagnitudR1,
+													   @PathVariable(name = "minMagnitudR2") String minMagnitudR2,
+													   @PathVariable(name = "maxMagnitudR2") String maxMagnitudR2) {
+		return new ResponseEntity<Sismos>(sismosService.getSismosByFourMagnitudes(minMagnitudR1, maxMagnitudR1,minMagnitudR2, maxMagnitudR2),
+				HttpStatus.OK);
+	}
+	/**
+	 * End point que publica el conteo de sismos por dos paises en dos fechas
+	 * @param pais1
+	 * @param pais2
+	 * @param fecha1
+	 * @param fecha2
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.GET, path = "/buscar/CountByDatesAndCountries/{pais1}/{pais2}/{fecha1}/{fecha2}")
+	public ResponseEntity<?> getSismosByTwoDatesAndTwoCountries(
+												@PathVariable(name = "pais1") String pais1,
+												@PathVariable(name = "pais2") String pais2,
+												@PathVariable(name = "fecha1") String fecha1,
+												@PathVariable(name = "fecha2") String fecha2) {
+		return new ResponseEntity<>(sismosService.getSismosByTwoDatesAndTwoCountries(pais1,pais2,fecha1,fecha2), HttpStatus.OK);
+	}
+	
+
 }
--- sismos/src/main/java/com/example/sismos/service/SismosService.java
+++ sismos/src/main/java/com/example/sismos/service/SismosService.java
@@ -1,6 +1,7 @@
 package com.example.sismos.service;
 
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,32 +16,89 @@ import com.example.sismos.model.Sismos;
 public class SismosService {
 	@Autowired
 	private RestTemplate restTemplate;
-
-	public Sismos getSismosByRangoFecha(String fechaInicio, String fechaTermino) {
+	
+	/**
+	 * Exponer endpoint para recibir 2 fechas, este se debe comunicar con un servicio que obtiene el detalle de los sismos entre los días pasados como parámetros.
+	 * @param fechaInicio
+	 * @param fechaTermino
+	 * @return	simos entre dos fechas
+	 */
+	public Sismos getSismosByDate(String fecha1, String fecha2) {
 		String url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={0}&endtime={1}";
-		return restTemplate.getForObject(MessageFormat.format(url, fechaInicio, fechaTermino), Sismos.class);
+		return restTemplate.getForObject(MessageFormat.format(url, fecha1, fecha2), Sismos.class);
 	}
-
+	
+	/**
+	 * Exponer endpoint que reciba dos magnitudes, el cual se debe comunicar con un servicio que obtiene el detalle de los sismos que se encuentran entre dichas magnitudes
+	 * @param minMagnitude
+	 * @param maxMagnitude
+	 * @return sismos entre dos magnitudes 
+	 */
 	public Sismos getSismosByMagnitude(String minMagnitude, String maxMagnitude) {
-		String url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude=6.5&maxmagnitude=7";
-		//String url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude={0}&maxmagnitude={1}";
+		String url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude={0}&maxmagnitude={1}";
 		return restTemplate.getForObject(MessageFormat.format(url, minMagnitude, maxMagnitude), Sismos.class);
 	}
 	
+	
+	/**
+	 * Exponer un endpoint que reciba 4 fechas, el cual se debe comunicar con un servicio que entregue los sismos ocurridos entre dichos rangos
+	 * @param fechaInicioR1
+	 * @param fechaTerminoR1
+	 * @param fechaInicioR2
+	 * @param fechaTerminoR2
+	 * @return
+	 */
+	public Sismos getSismosByTwoDateRanges(String fechaInicioR1,String fechaTerminoR1,String fechaInicioR2,String fechaTerminoR2) {
+		String url1 = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={0}&endtime={1}";
+		String url2 = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={2}&endtime={3}";
+		return restTemplate.getForObject(MessageFormat.format(url1 + " / " + url2, fechaInicioR1, fechaTerminoR1,fechaInicioR2,fechaTerminoR2), Sismos.class);
+	}
+	
+	/**
+	 * Se deben obtener los sismos ocurridos entre el 1 y 3 de Octubre más los ocurridos entre el 6 y 14 de Octubre. 
+	 * @param minMagnitudeR1
+	 * @param maxMagnitudeR1
+	 * @param minMagnitudeR2
+	 * @param maxMagnitudeR2
+	 * @return
+	 */
+	public Sismos getSismosByFourMagnitudes(String minMagnitudeR1, String maxMagnitudeR1,String minMagnitudeR2, String maxMagnitudeR2 ) {
+		String url1 = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude={0}&maxmagnitude={1}";
+		String url2 = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude={2}&maxmagnitude={3}";
+		return restTemplate.getForObject(MessageFormat.format(url1 + " / " + url2, minMagnitudeR1, maxMagnitudeR1,minMagnitudeR2,maxMagnitudeR2), Sismos.class);
+	}
+		
+	/*
+	 * Exponer un endpoint para recibir un país como parámetro, el cual se debe comunicar con un servicio que filtra todos los sismos y los retorna.
+	 * @param pais
+	 * @return sismos por pais
+	 */
 	public List<Feature> getSismosByPais(String pais) {
 		String url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson";
 		Sismos sismos = restTemplate.getForObject(url, Sismos.class);
 		return sismos.getFeatures().parallelStream().filter(f -> f.getProperties().getPlace().contains(", " + pais))
 				.collect(Collectors.toList());
 	}
-	
-	
-	public Sismos getSismosByTwoDateRanges(String fechaInicioR1,String fechaTerminoR1,String fechaInicioR2,String fechaTerminoR2) {
-		String url1 = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={0}&endtime={1}";
-		String url2 = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={2}&endtime={3}";
-		return restTemplate.getForObject(MessageFormat.format(url1 + "Richard" + url2, fechaInicioR1, fechaTerminoR1,fechaInicioR2,fechaTerminoR2), Sismos.class);
+		
+	/**
+	 * Exponer endpoint para recibir 2 países y dos fechas, se debe comunicar con un servicio para contar la cantidad de sismos ocurridos en los datos que cumplan con los parámetros dados. 
+	 * @param pais1
+	 * @param pais2
+	 * @param fecha1
+	 * @param fecha2
+	 * @return conteo de sismos totales filtrados por fechas y paises
+	 */
+	public int getSismosByTwoDatesAndTwoCountries(String pais1, String pais2, String fecha1,String fecha2) {
+		String url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson";
+		Sismos sismos = restTemplate.getForObject(MessageFormat.format(url,fecha1, fecha2), Sismos.class);
+		List<Feature> sismos1 = sismos.getFeatures().parallelStream().filter(f -> f.getProperties().getPlace().contains(", " + pais1)).collect(Collectors.toList());
+		List<Feature> sismos2 = sismos.getFeatures().parallelStream().filter(f -> f.getProperties().getPlace().contains(", " + pais2)).collect(Collectors.toList());
+		List<Feature> sismoTotal = new ArrayList<Feature>();
+		sismoTotal.addAll(sismos1);
+		sismoTotal.addAll(sismos2);
+			
+		return sismoTotal.size();
+					
 	}
-
-	
 	
 }
