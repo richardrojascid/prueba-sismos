c671f425e3f2157a7d614fd8070fc76bad4985ea
richardrojascid <richardrojascid>
2019-12-05 15:17:47 -0300

    cambios con configuracion a sismos-services

--- sismos/src/main/java/com/example/sismos/controllers/SismosController.java
+++ sismos/src/main/java/com/example/sismos/controllers/SismosController.java
@@ -1,8 +1,17 @@
 package com.example.sismos.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.sismos.model.Sismos;
 import com.example.sismos.service.SismosServiceImpl;
 
+@RefreshScope
 @RestController
 @RequestMapping("sismos")
 public class SismosController {
+	
+	private static  Logger log = LoggerFactory.getLogger(SismosController.class);
 
 	/**
 	 * sismosService: se injecta este Servicio para obtener la información de la URL
@@ -26,8 +38,34 @@ public class SismosController {
 	 */
 
 	@Autowired
+	private Environment env;
+	
+	@Autowired
 	private SismosServiceImpl sismosService;
+	
+	@Value("${configuracion.texto}")
+	private String texto;
 
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}")String puerto){
+		
+		log.info(texto);
+		
+		Map<String,String> json = new HashMap<>();
+				json.put("texto", texto);
+				json.put("puerto", puerto);
+		
+		
+		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+}
+	
+	
 	/**
 	 * Método mediante servicio sismosService y publica la información de sismos por
 	 * fechas en el EndPoit local.
