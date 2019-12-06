ad7ab6f8ee8c31944effe07dfbefc61b80788751
richardrojascid <richardrojascid>
2019-12-05 22:11:20 -0300

    Se crea servicio servicio-config en git local

---
+++ sismos-servicio-config-server/src/main/java/com/example/demo/config/SismosServicioConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SismosServicioConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SismosServicioConfigServerApplication.class, args);
+	}
+
+}
---
+++ sismos-servicio-config-server/src/test/java/com/example/demo/config/SismosServicioConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SismosServicioConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
